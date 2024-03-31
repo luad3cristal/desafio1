80b4c43ebfa198ee053652a09d6d5b83459a7c34
Luana <luanalima.2006@hotmail.com>
2024-03-31 15:13:09 -0300

    last commit

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const heart = document.querySelector("#heart")
-const heart1 = document.querySelector("#heart1")
-const heart2 = document.querySelector("#heart2")
+let heart = document.querySelector("#heart")
+let heart1 = document.querySelector("#heart1")
+let heart2 = document.querySelector("#heart2")
+const c1 = heart.getAttribute("name")
 
 heart.onclick = function () {
   heart.setAttribute("name", "heart")
+}
+
+heart.ondblclick = function () {
+  heart.setAttribute("name", "heart-outline")
 }
 
 heart1.onclick = function () {
   heart1.setAttribute("name", "heart")
 }
 
+heart1.ondblclick = function () {
+  heart1.setAttribute("name", "heart-outline")
+}
+
 heart2.onclick = function () {
   heart2.setAttribute("name", "heart")
+}
+
+heart2.ondblclick = function () {
+  heart2.setAttribute("name", "heart-outline")
 }
