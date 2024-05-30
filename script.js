a2e62e49e77669eee6241ad730b03da6455fd37e
Luana <luanalima.2006@hotmail.com>
2024-05-30 17:50:45 -0300

    altering javascript 1.1

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let heart = document.querySelector("#heart")
 let heart1 = document.querySelector("#heart1")
 let heart2 = document.querySelector("#heart2")
-const c1 = heart.getAttribute("name")
+let c1 = heart.getAttribute("name")
 
-heart.onclick = function () {
-  heart.setAttribute("name", "heart")
+if (heart.getAttribute("name") == "heart-outline") {
+  heart.onclick = function () {
+    heart.setAttribute("name", "heart")
+  }
 }
 
-heart.ondblclick = function () {
-  heart.setAttribute("name", "heart-outline")
+// if (heart.getAttribute("name") == "heart") {
+//   heart.onclick = function () {
+//     heart.setAttribute("name", "heart-outline")
+//   }
+// }
+
+if (heart1.getAttribute("name") == "heart-outline") {
+  heart1.onclick = function () {
+    heart1.setAttribute("name", "heart")
+  }
 }
 
-heart1.onclick = function () {
-  heart1.setAttribute("name", "heart")
-}
-
-heart1.ondblclick = function () {
-  heart1.setAttribute("name", "heart-outline")
-}
-
-heart2.onclick = function () {
-  heart2.setAttribute("name", "heart")
-}
-
-heart2.ondblclick = function () {
-  heart2.setAttribute("name", "heart-outline")
+if (heart2.getAttribute("name") == "heart-outline") {
+  heart2.onclick = function () {
+    heart2.setAttribute("name", "heart")
+  }
 }
